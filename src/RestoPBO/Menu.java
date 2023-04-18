ba8d1184d0fef7364902a325213fbb9ac5012093
NandaFebian <nandafebiandananjaya@gmail.com>
2023-04-18 17:15:00 +0800

    Menghapus class admin, merombak class semua class, menambahkan dan memperbaiki fitur-fitur yang salah

---
+++ src/RestoPBO/Menu.java
@@ -0,0 +1,20 @@
+package RestoPBO;
+
+public class Menu {
+    private String name;
+    private int price;
+
+    public Menu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
+
